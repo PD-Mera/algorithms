efed88557aca5c85c63d7b18bfc7b093f338f878
Trinh Phuong Dong <phuongdong1772000@gmail.com>
2024-01-12 11:15:44 +0700

    add reversed

---
+++ README.md
@@ -0,0 +1,12 @@
+# Self-implement of sort algorithms
+
+- [x] Bubble Sort
+- [ ] Insertion Sort
+- [ ] Selection Sort
+- [ ] Shell Sort
+- [ ] Merge Sort
+- [ ] Quick Sort
+- [ ] Heap Sort
+- [ ] Counting Sort
+- [ ] Radix Sort
+- [ ] Bucket Sort
--- mlib_sort/bubble_sort.py
+++ mlib_sort/bubble_sort.py
@@ -1,15 +1,20 @@
-def bubble_sort(input_list: list) -> list:
+def bubble_sort(input_list: list, reversed: bool = False) -> list:
     sorted_list = input_list.copy()
     element_number = len(sorted_list)
     for idx1 in range(element_number - 1):
         for idx2 in range(idx1 + 1, element_number):
             if sorted_list[idx1] < sorted_list[idx2]:
                 sorted_list[idx1], sorted_list[idx2] = sorted_list[idx2], sorted_list[idx1]
+    if reversed:
+        sorted_list = sorted_list[::-1]
     return sorted_list
+
 
 if __name__ == "__main__":
     import random
     input_list = [random.randint(1, 100) for _ in range(10)]
     sorted_list = bubble_sort(input_list)
+    reversed_sorted_list = bubble_sort(input_list, reversed = True)
     print(input_list)
     print(sorted_list)
+    print(reversed_sorted_list)
